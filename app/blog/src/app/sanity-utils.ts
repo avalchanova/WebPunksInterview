fdbde3ed7698c4d1e09f680e835a23b140ede85a
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-18 21:07:51 +0300

    cleanup

--- app/blog/src/app/sanity-utils.ts
+++ app/blog/src/app/sanity-utils.ts
@@ -3,14 +3,16 @@ import { clientConfig } from "./sanity-config";
 import { Page } from "../../types/Page";
 import { Post } from "../../types/Post";
 
-const asd = `*[_type == "page" && slug.current == $slug][0]{
-  _id,
-  title,
-  content[]{
-  "children": children[0].text
-  },
-  "slug": slug.current
-}`
+// GROQ
+
+// const asd = `*[_type == "page" && slug.current == $slug][0]{
+//   _id,
+//   title,
+//   content[]{
+//   "children": children[0].text
+//   },
+//   "slug": slug.current
+// }`
 
 export async function getPage(slug: string): Promise<Page> {
   return createClient(clientConfig).fetch(
@@ -37,7 +39,7 @@ export async function getPages(): Promise<Page[]> {
   )
 } // this is for the navbar
 
-// grabs everything for the post content
+
 export async function getPost(slug: string): Promise<Post> {
   return createClient(clientConfig).fetch(
     groq`
@@ -60,10 +62,10 @@ export async function getPost(slug: string): Promise<Post> {
     }
     `,
     { slug: slug }
-  );
+  ); // grabs everything for the post content
 }
 
-// grabs all of the posts to display in the homepage
+
 export async function getPosts(): Promise<Post[]> {
 
   return createClient(clientConfig).fetch(
@@ -83,7 +85,7 @@ export async function getPosts(): Promise<Post[]> {
         metaDescription,
         "slug": slug.current
       }
-  }`
+  }` // grabs all of the posts to display in the homepage
 
   )
 
