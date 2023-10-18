f6a1c384d89ed1bec034c5127daa447f450b18c5
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-18 20:25:06 +0300

    page and post schemas

--- app/cms/schemas/index.ts
+++ app/cms/schemas/index.ts
@@ -1,3 +1,4 @@
-import { postSchemas } from "./post"
+import pageSchemas from "./page-schema"
+import { postSchemas } from "./post-schema"
 
-export const schemaTypes = [...postSchemas]
+export const schemaTypes = [...postSchemas, pageSchemas]
