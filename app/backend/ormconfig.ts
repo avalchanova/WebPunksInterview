a1d6849829a3a1e53d74155f159f91cf17e01631
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-19 20:05:51 +0300

    fix config backend typeorm

--- app/backend/ormconfig.ts
+++ app/backend/ormconfig.ts
@@ -1,6 +1,4 @@
-import { Comment } from "src/entities/comment.entity"
-import { Topic } from "src/entities/topic.entity"
-import { User } from "src/entities/user.entity"
+import { TrackPageViewsEntity } from "src/entities/TrackPageViewsEntity.entity"
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions"
 
 const config: PostgresConnectionOptions = {
@@ -12,7 +10,7 @@ const config: PostgresConnectionOptions = {
   password: "wp-pwd",
   //adding username and pass is not good practise
   //todo: move them as a environment variable file and add them here
-  entities: [User, Topic, Comment],
+  entities: [TrackPageViewsEntity],
   // if we have other entities, add them here
   synchronize: true,
   // in development: when we commit a change into one of our models, the typeorm will commit these changes into our DB system
