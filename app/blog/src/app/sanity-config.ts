b03da0aa8c65655838f3a560d8a732d735d2f4ba
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-20 20:40:23 +0300

    refactor and styling

--- app/blog/src/app/sanity-config.ts
+++ app/blog/src/app/sanity-config.ts
@@ -1,5 +1,6 @@
 export const clientConfig = {
-  projectId: '2tkojebt',
-  dataset: 'production',
+  projectId: "2tkojebt",
+  dataset: "production",
   useCdn: false,
-};
+  apiVersion: "2022-02-03",
+}
--- app/blog/types/Post.ts
+++ app/blog/types/Post.ts
@@ -1,37 +1,36 @@
-import { PortableTextBlock } from "sanity";
 // types lay in the next project because we will be using the while we query the posts (for instance) and we need to have a return type; displaying it
 // Define types for the post schema
 export type Post = {
-  _id: string;
-  _type: "post";
-  _createdAt: string;
-  contents: PostContents;
-  seoData: PostMetadata;
+  _id: string
+  _type: "post"
+  _createdAt: string
+  contents: PostContents
+  seoData: PostMetadata
   // Add any other fields specific to your post document
 }
 
 // Define types for the postContents schema
-type PostContents = {
-  title: string;
-  body: string;
-  cover: PostCover;
+export type PostContents = {
+  title: string
+  body: string
+  cover: PostCover
   // Add any other fields specific to your postContents object
 }
 
 // Define types for the postCover schema
-type PostCover = {
-  mobile: string;
-  tablet: string;
-  desktop: string;
+export type PostCover = {
+  mobile: string
+  tablet: string
+  desktop: string
   // Add any other fields specific to your postCover object
 }
 
 // Define types for the postMetadata schema
 export type PostMetadata = {
-  slug: string;
-  metaTitle: string;
-  metaDescription: string;
+  slug: string
+  metaTitle: string
+  metaDescription: string
   // Add any other fields specific to your postMetadata object
 }
 
-export type SlugOnly = Pick<PostMetadata, 'slug'>; // only the slug
+export type SlugOnly = Pick<PostMetadata, "slug"> // only the slug
