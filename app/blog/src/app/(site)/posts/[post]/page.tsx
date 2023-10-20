cdc5615b1d8d89ecd2d3bc4d7595ea6431cdc671
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-20 20:36:36 +0300

    implement ImageRenderer to single post page

--- app/blog/src/app/(site)/posts/[post]/page.tsx
+++ app/blog/src/app/(site)/posts/[post]/page.tsx
@@ -1,60 +1,52 @@
-import { Metadata } from 'next'
-import {  getPost, getPostMeta } from '@/app/utils/sanity-utils';
+import { Metadata } from "next"
+import { getPost, getPostMeta } from "@/app/utils/sanity-utils"
 import Image from "next/image"
-import { SlugOnly } from '../../../../../types/Post';
+import { SlugOnly } from "../../../../../types/Post"
+import ImageRenderer from "@/app/components/ImageRenderer/ImageRenderer"
 
 type Props = {
-    params: {post: SlugOnly};
-    searchParams: { [key: string]: string | string[] | undefined };
-};
+  params: { post: SlugOnly }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
 
-
-export async function generateMetadata(
-  { params }: Props,
-): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = params.post
-    // fetch data
+  // fetch data
   const metaObject = await getPostMeta(slug)
 
   return {
-      title: metaObject.metaTitle,
-      description: metaObject.metaDescription
+    title: metaObject.metaTitle,
+    description: metaObject.metaDescription,
   }
 }
 
+export default async function Post({ params }: Props) {
+  const slug = params.post // this is called "post" because the folder is "[post]" and this is actually the post's slug
+  const post = await getPost(slug)
 
-export default async function Post({params}: Props){
-    const slug = params.post // this is called "post" because the folder is "[post]" and this is actually the post's slug
-    const post = await getPost(slug)
-
-    return (
-    <div>
+  return (
+    <div className="flex flex-1 justify-evenly min-h-fit">
+      <div className="flex max-w-[50%] items-center">
+        <ImageRenderer
+          priority={true}
+          src={post.contents.cover}
+          alt={post.contents.title}
+          width={1920}
+          height={1080}
+          className="mt-10 object-cover rounded-xl"
+        />
+      </div>
+      <div className="flex flex-col flex-1 pl-20 justify-center">
         <header className="flex items-center justify-between">
-        <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">{post.contents.title}</h1>
-        {/* <a
-            href={post.url}
-            title="Edit"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition">
-            Edit
-        </a> */}
-        {/* this <a> button can be made an edit button but it's just an idea */}
+          <h1 className="py-5 font-bold font-semibold text-gray-900 text-6xl">
+            {post.contents.title}
+          </h1>
         </header>
 
         <div className="text-lg text-gray-700 mt-5">
-        <p className="whitespace-pre-wrap">{post.contents.body}</p>
+          <p className="whitespace-pre-wrap">{post.contents.body}</p>
         </div>
-
-        <Image
-            src={post.contents.cover.desktop}
-            alt={post.contents.title}
-            width={1920}
-            height={1080}
-            priority={true}
-            className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
-            />
+      </div>
     </div>
-    )
+  )
 }
-
