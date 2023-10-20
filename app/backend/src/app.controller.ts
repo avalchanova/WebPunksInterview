abc136367a3973eb5ddf9a0094e2e2d932b803de
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-20 20:29:01 +0300

    basic cleanup in backend and autogenerated api-spec, jest-e2e

--- app/backend/src/app.controller.ts
+++ app/backend/src/app.controller.ts
@@ -1,28 +1,6 @@
-import { Controller, Get, Res } from "@nestjs/common"
-import { ApiOperation } from "@nestjs/swagger"
-import { AppService } from "./app.service"
+import { Controller } from "@nestjs/common"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  // @Get() // this is a decorator (when there is a @ - decorator)
-  // getHello(): string {
-  //   return this.appService.getHello()
-  // }
-
-  @Get()
-  index(@Res() res: any) {
-    return res.redirect("swagger")
-  }
-
-  @Get("health")
-  @ApiOperation({
-    operationId: "healthCheck",
-  })
-  health() {
-    return {
-      healthy: true,
-    }
-  }
+  constructor() {}
 }
