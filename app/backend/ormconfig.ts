e6c9a1a800de8df3aaffb66caf3fd22d0f769494
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-16 21:29:32 +0200

    add .env and move all hard coded configs there and prepare for heroku deploy with config for yarn package manager inside it

--- app/backend/ormconfig.ts
+++ app/backend/ormconfig.ts
@@ -1,18 +1,20 @@
 import { TrackPageViewsEntity } from "src/entities/TrackPageViewsEntity.entity"
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const config: PostgresConnectionOptions = {
   type: "postgres",
-  database: "wp-core",
-  host: "localhost",
-  port: 5432,
-  username: "wp-usr",
-  password: "wp-pwd",
+  database: process.env.DB_NAME || "wp-core",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || "wp-usr",
+  password: process.env.DB_PASSWORD || "wp-pwd",
   //adding username and pass is not good practise
   //todo: move them as a environment variable file and add them here
   entities: [TrackPageViewsEntity],
   // if we have other entities, add them here
-  synchronize: true,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === "true" || false,
   // in development: when we commit a change into one of our models, the typeorm will commit these changes into our DB system
   // however, in production environment must be false, because we may lose our data
 }
--- resume.md
+++ resume.md
@@ -1,3 +1,4 @@
+
 This project is a multi-part web development initiative that showcases my proficiency in various web technologies. It is designed to demonstrate my skills in building a content management system (CMS) application, creating a backend REST API, and developing a frontend application using React and Next.js.
 
 Project Components:
